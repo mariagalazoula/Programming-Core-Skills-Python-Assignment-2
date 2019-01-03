9dc8174b5f47d047b1097995c3fbf131c47decc2
Maria Galazoula <43342073+mariagalazoula@users.noreply.github.com>
2019-01-03 20:00:49 +0200

    Update Model_for_drunks.py

--- Model_for_drunks.py
+++ Model_for_drunks.py
@@ -5,11 +5,9 @@ Created on Thu Nov 15 16:28:53 2018
 @author: gy16mg
 """
 
-
 #import the modules that we will use
 #the csv module will be needed so that we can read 
 #the data for the environment
-
 
 import csv
 import matplotlib
@@ -33,7 +31,7 @@ for row in reader:
     #for each of the rows read the values and write them in the list above
     for value in row:
         #fill the empty rowlist with values from each one of the rows
-        #change the value of the pub to 255
+        #change the value of the pub from 1 to 255
         if (value == 1):
             rowlist.append(255)
         else:
@@ -51,7 +49,9 @@ matplotlib.pyplot.show()
 
 """In this section, the density map will be created.
 """
-#create the environment initially as an empty list
+#create the environment initially as an empty list, 
+#the same way the environment was created.
+
 density_map = []
 
 #read each row in the reader 
@@ -72,9 +72,11 @@ matplotlib.pyplot.show()
 
 """In this section, we will find the coordinates of the pub.
 """
+
 #write code that will find the location of the pub
 #we have to go through all the values 
-#in order to find the 1s for the pub
+#in order to find the 255s for the pub
+
 pub = []
 for j in range(len(environment)):
     for i in range(len(environment[j])):
@@ -89,9 +91,7 @@ print(pub)
 
 """In this section, we will generate the drunks
    and make them move.
-   
 """
-
 
 num_of_drunks = 25
 drunks= []
@@ -101,6 +101,7 @@ y = pub[0]
 #create a list of drunks using the Drunks_Framework.py
 for i in range(num_of_drunks):
     drunks.append(drfr.Drunk(x, y, (i+1), environment, drunks))
+
 #move the agents while the condition
 # "drunks[i].is_drunk_home" is False
 #when this becomes true, the next agent starts moving
@@ -109,11 +110,13 @@ for i in range(num_of_drunks):
             drunks[i].move()
             density_map[drunks[i].y][drunks[i].x] = density_map[drunks[i].y][drunks[i].x] + 1
             
-
+#plot the density map that is created from the movement of drunks
 matplotlib.pyplot.imshow(density_map)
 matplotlib.pyplot.show()
           
 #write the density map in a new txt file named 
+#"Density_map.txt"
+
 f2 = open('Density_map.txt', 'w', newline='') 
 writer = csv.writer(f2, delimiter=' ')
 for row in density_map:		
